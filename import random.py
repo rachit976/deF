d8fcf311745d24201ec21f2bbb5e531fd08e1eb7
rachit976 <mukeshsoni38@gmail.com>
2025-04-12 20:21:55 +0530

    Add files via upload

---
+++ import random.py
@@ -0,0 +1,12 @@
+import random 
+playing = Treue
+number = str(random.randint(10,20))
+print("i will generate a number from 0 to 9, and you havee to guess the nuumber one digit at a time")
+print(the game ends when you get 1 hero!)
+      while playing:
+      guess:
+      print("you win the game")
+      print("the number was",numberbbreak
+            
+            else:
+                pint("your guess isnt quite right , try again.\n"))
